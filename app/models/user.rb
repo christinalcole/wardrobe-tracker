eb6423bf7fdb9511a417458445eb23a502c27595
Shannon McCormack <shannonkmc@hotmail.com>
2017-07-18 19:43:05 +0000

    Added slug and find_by_slug methods to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ActiveRecord::Base
   validates :username, presence: true
   validates :email, presence: true
   validates :password, presence: true
+
+  def slug
+    self.username.strip.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    name = slug.split("-").map { |word| word.downcase }.join(" ")
+    self.all.find { |instance| instance.username.downcase == name }
+  end
+
 end

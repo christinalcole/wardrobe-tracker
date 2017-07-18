0f87ea49dedeb362f532b8da8f7e3590c193736f
Shannon McCormack <shannonkmc@hotmail.com>
2017-07-18 20:24:32 +0000

    Added form content to user views. Troubleshoot code in controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,74 @@
 class UsersController < ApplicationController
 
   # GET: /users
-  get "/users" do
-    erb :"/users/index.html"
+  get "/users/:slug" do
+    @user = User.find_by_slug(params[:slug])
+    binding.pry
+    erb :"users/show"
   end
 
-  # GET: /users/new
-  get "/users/new" do
-    erb :"/users/new.html"
+  # GET: /users/signup
+  get "/signup" do
+    if !logged_in?
+      flash[:message] = "You need to complete all the required fields."
+      erb :"/users/signup"
+    else
+      redirect to '/wardrobes'
+    end
   end
 
-  # POST: /users
-  post "/users" do
-    redirect "/users"
+  # POST: /signup
+  post "/signup" do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect "/signup"
+    else
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/wardrobes'
+    end
   end
 
-  # GET: /users/5
-  get "/users/:id" do
-    erb :"/users/show.html"
+  # GET: /login
+    get '/login' do
+      if !logged_in?
+        erb :'users/login'
+      else
+        redirect '/wardrobes'
+      end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/wardrobes"
+    else
+      redirect to '/signup'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
   end
 
   # GET: /users/5/edit
-  get "/users/:id/edit" do
-    erb :"/users/edit.html"
-  end
+  # get "/users/:id/edit" do
+  #   erb :"/users/edit.html"
+  # end
 
   # PATCH: /users/5
-  patch "/users/:id" do
-    redirect "/users/:id"
-  end
+  # patch "/users/:id" do
+  #   redirect "/users/:id"
+  # end
 
   # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
-  end
+  # delete "/users/:id/delete" do
+  #   redirect "/users"
+  # end
 end
